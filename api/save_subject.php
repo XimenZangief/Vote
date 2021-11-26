e5d00d33fdff85a29a5dc2dca0a5431640f32726
XimenZangief <s9671073@gmail.com>
2021-11-27 04:45:31 +0800

    表單：標題+選項儲存

--- api/save_subject.php
+++ api/save_subject.php
@@ -1,5 +1,21 @@
 <?php
 
+include_once "./db.php";
 print_r($_POST['subject']);
 
-?>
+//資料表的欄位名稱=>資料內容
+$topic_array = ['topic' => $_POST['subject']];
+ins('topics', $topic_array);
+$topic_sql = "SELECT * from `topics` where `topic`='{$_POST['subject']}'";
+$topic = $pdo->query($topic_sql)->fetch();
+
+foreach ($_POST['options'] as $key) {
+  $opt_array = ['opt'=>$key, 'topic_id'=>$topic['id']];
+  ins('options', $opt_array);
+}
+$opt_sql = "SELECT * from `options` where `id`='{$POST['subject']}'";
+$opt = $pdo->query($opt_sql)->fetch();
+
+echo $topic;
+echo '<hr>';
+echo $opt;
