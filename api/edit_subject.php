fa406ef30a98caade16c4c1c04bf0929210d4360
XimenZangief <s9671073@gmail.com>
2021-11-29 16:39:02 +0800

    edit_subject.api 若有value則修改option，無value則新增option backend edit選項不足4個則補到4個

--- api/edit_subject.php
+++ api/edit_subject.php
@@ -1,26 +1,21 @@
 編輯
 <?php
+include_once "db.php";
+$topic=$_POST['topic'];
+$topic_id=$_POST['topic_id'];
 
-$subject=select_one('topics', $_GET['id']);
-$options=select_all('options',['topic_id'=>$_GET['id']]);
-?>
 
-<!-- $subject['topic']是欄位名稱 -->
-<form action="../api/save_subject.php" method="POST" class="col-6 m-auto p-2">
-    <label>主題: <input type="text" name="subject" value="<?= $subject['topic']; ?>"></label> 
-    <label class="list-group-item">option1: <input type="text" name="options[]"></label>
-    <label class="list-group-item">option2: <input type="text" name="options[]"></label>
-    <label class="list-group-item">option3: <input type="text" name="options[]"></label>
-    <label class="list-group-item">option4: <input type="text" name="options[]"></label>
-    <label class="list-group-item">option5: <input type="text" name="options[]"></label>
-    <?php
-    foreach ($options as $opt) {
-        echo "<label class='list-group-item'>";
-        echo "選項" . ($key+1);
-        echo "<input type='text' name='options[]' value='{$opt['opt']}'>";
-        echo "</label>";
+update('topics',['topic'=>$topic],['id'=>$topic_id]);
+
+$options=$_POST['options'];
+$opt_id=$_POST['opt_id'];
+
+foreach ($options as $key => $opt) {
+    if(array_key_exists($key,$opt_id)){
+        update('options',['opt'=>$opt],['id'=>$opt_id[$key]]);
+    }else{
+        ins('options',['opt'=>$opt, 'topic_id'=>$topic_id]);
     }
+}
 
 ?>
-    <button type="submit" class="btn btn-primary">Submit</button>
-  </form>
--- index.php
+++ index.php
@@ -12,23 +12,42 @@
 </head>
 
 <body>
-<?php
 
-$do=(isset($_GET['do'])) ? $_GET['do'] : 'show_vote_list';
-$file="./frontend/" .$do.".php";
-if(file_exists($file)){
-    include $file;
-}else{
-    include "./frontend/show_vote_list.php";
-}
-?>
-<div class="bg-primary fixed-bottom">my iron body is invincible, so be ware</div>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
-    </script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous">
-    </script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous">
-    </script>
+    <div class="jumbotron p-0" style="overflow:hidden;height:250px">
+        <a href="index.php">
+            <div id="carouselExampleSlidesOnly" class="carousel slide position-relative" data-ride="carousel">
+                <div class="carousel-inner position-absolute" style="top:-250px">
+                    <div class="carousel-item active">
+                        <img class="d-block w-100 bg-primary" src="" alt="First slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100 bg-secondary" src="" alt="Second slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100 bg-info" src="" alt="Third slide">
+                    </div>
+                </div>
+            </div>
+        </a>
+    </div>
+    <div class="container">
+        <?php
+
+        $do = (isset($_GET['do'])) ? $_GET['do'] : 'show_vote_list';
+        $file = "./frontend/" . $do . ".php";
+        if (file_exists($file)) {
+            include $file;
+        } else {
+            include "./frontend/show_vote_list.php";
+        }
+        ?>
+        <div class="bg-primary fixed-bottom">my iron body is invincible, so be ware</div>
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+        </script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous">
+        </script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous">
+        </script>
 </body>
 
 </html>
